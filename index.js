d0ce26d8b3c13d13886668f423ac9d64faa4dda3
AndrewSB <asbreckenridge@me.com>
2014-10-18 03:56:22 -0400

    added bro

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World LOL!')
+  response.send('Hello World LOLBro!')
 })
 
 app.listen(app.get('port'), function() {
