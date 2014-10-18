bcb228ec00e1c26aac8cdd04435a7fd1591990f5
Hilal Habashi <habashi2@illinois.edu>
2014-10-18 13:38:40 -0400

    Now Works

--- index.js
+++ index.js
@@ -33,36 +33,36 @@ app.post('/incoming', function(request, response) {
        response.send(twiml, {'Content-Type':'text/xml'}, 200);
 });
 
-// app.listen(app.get('port'), function() { //   console.log("Node app is running at localhost:" + app.get('port')) // })
-var rapgeniusClient = require("rapgenius-js");
+//// app.listen(app.get('port'), function() { //   console.log("Node app is running at localhost:" + app.get('port')) // })
+//var rapgeniusClient = require("rapgenius-js");
 
-var lyricsSearchCb = function(err, lyricsAndExplanations){
-    if(err){
-      console.log("Error: " + err);
-    }else{
-      //Printing lyrics with section names
-      var lyrics = lyricsAndExplanations.lyrics;
-      var explanations = lyricsAndExplanations.explanations;
-      console.log("Found lyrics for song [title=%s, main-artist=%s, featuring-artists=%s, producing-artists=%s]",
-        lyrics.songTitle, lyrics.mainArtist, lyrics.featuringArtists, lyrics.producingArtists);
-      console.log("**** LYRICS *****\n%s", lyrics.getFullLyrics(true));
+//var lyricsSearchCb = function(err, lyricsAndExplanations){
+    //if(err){
+      //console.log("Error: " + err);
+    //}else{
+      ////Printing lyrics with section names
+      //var lyrics = lyricsAndExplanations.lyrics;
+      //var explanations = lyricsAndExplanations.explanations;
+      //console.log("Found lyrics for song [title=%s, main-artist=%s, featuring-artists=%s, producing-artists=%s]",
+        //lyrics.songTitle, lyrics.mainArtist, lyrics.featuringArtists, lyrics.producingArtists);
+      //console.log("**** LYRICS *****\n%s", lyrics.getFullLyrics(true));
 
-      //Now we can embed the explanations within the verses
-      // lyrics.addExplanations(explanations);
-      // var firstVerses = lyrics.sections[0].verses[0];
-      // console.log("\nVerses:\n %s \n\n *** This means ***\n%s", firstVerses.content, firstVerses.explanation);
-    }
-};
+      ////Now we can embed the explanations within the verses
+      //// lyrics.addExplanations(explanations);
+      //// var firstVerses = lyrics.sections[0].verses[0];
+      //// console.log("\nVerses:\n %s \n\n *** This means ***\n%s", firstVerses.content, firstVerses.explanation);
+    //}
+//};
 
-var searchCallback = function(err, songs){
-  if(err){
-    console.log("Error: " + err);
-  }else{
-    if(songs.length > 0){
-      //We have some songs
-      rapgeniusClient.searchLyricsAndExplanations(songs[0].link, "rap", lyricsSearchCb);
-    }
-  }
-};
+//var searchCallback = function(err, songs){
+  //if(err){
+    //console.log("Error: " + err);
+  //}else{
+    //if(songs.length > 0){
+      ////We have some songs
+      //rapgeniusClient.searchLyricsAndExplanations(songs[0].link, "rap", lyricsSearchCb);
+    //}
+  //}
+//};
 
-rapgeniusClient.searchSong("Rap God", "rap", searchCallback);
+//rapgeniusClient.searchSong("Rap God", "rap", searchCallback);
