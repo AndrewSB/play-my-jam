8666ade53e8bbdbcbeaf99564024231c4415247b
AndrewSB <asbreckenridge@me.com>
2014-10-18 22:40:49 -0400

    add gems and new twiilio.rb

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require 'rapgenius'
 require 'sinatra'
 require 'pp'
 require 'json'
+require 'soundcloud'
 
 post '/search_by_lyrics' do
 	query = params["query"]
@@ -19,4 +20,10 @@ post '/search_by_title' do
 	strings = RapGenius.search_by_title(query)[0..5].collect do | song |
 		puts song
 	end
+end
+
+post '/get_url_from_track' do 
+	query = params["query"]
+	client = SoundCloud.new(:client_id => "5d2e84914f361b7350f4f7ece1b223fa")
+	
 end
