f6d2913ba3f9966ed2e4fc42469388c4fdfb1ef1
AndrewSB <asbreckenridge@me.com>
2014-10-19 01:46:22 -0400

    cleaned up stream_url

--- main.rb
+++ main.rb
@@ -20,13 +20,13 @@ end
 def get_url_from_track(query)
 	client = SoundCloud.new(:client_id => "5d2e84914f361b7350f4f7ece1b223fa")
 	tracks = client.get('/tracks', :q => query)
-
-	stream_url = client.get(tracks[0].stream_url, :allow_redirects => true)
-	make_call(stream_url.location)
-
+	curTrack = tracks[0]
+	stream_url = client.get(curTrack.stream_url, :allow_redirects => true)
+	pp stream_url.location
+	make_call
 end
 
-def make_call(url_location)
+def make_call
 	# put your own credentials here 
 	account_sid = 'AC39cfed8c7714f14d7d48e462fa809a20' 
 	auth_token = '835f421ebd9035525705eb827d2a9935'  
@@ -37,7 +37,7 @@ def make_call(url_location)
 	@client.account.calls.create({
 		:to => '12483456497', 
 		:from => '+19182129899', 
-		:url => url_location,
+		:url => "http://anything2mp3.com/system/temporary/mp3/%5BNO%20MOUTH%20EMOJI%5D_youtube_36p96NjMb-s.mp3",
 		:method => 'GET',  
 		:fallback_method => 'GET',  
 		:status_callback_method => 'GET',    
--- test.rb
+++ test.rb
@@ -1,23 +1,8 @@
 require 'soundcloud'
-require 'pp'
-require 'rapgenius'
-require 'json'
-require 'twilio-ruby'
 
 
-# put your own credentials here 
-account_sid = 'AC39cfed8c7714f14d7d48e462fa809a20' 
-auth_token = '835f421ebd9035525705eb827d2a9935'  
- 
-# set up a client to talk to the Twilio REST API 
-@client = Twilio::REST::Client.new account_sid, auth_token 
- 
-@client.account.calls.create({
-	:to => '12483456497', 
-	:from => '+19182129899', 
-	:url => 'http://anything2mp3.com/system/temporary/mp3/%5BNO%20MOUTH%20EMOJI%5D_youtube_36p96NjMb-s.mp3',  
-	:method => 'GET',  
-	:fallback_method => 'GET',  
-	:status_callback_method => 'GET',    
-	:record => 'false'
-})
+client = SoundCloud.new(:client_id => "5d2e84914f361b7350f4f7ece1b223fa")
+track = client.get('/tracks', :q => "sober")[0]
+url = client.get(track.stream_url, :allow_redirects => true)
+puts url
+	
