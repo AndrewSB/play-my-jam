9f5493855f972f1375c87dc4d7ff0ee6577ddfe4
AndrewSB <asbreckenridge@me.com>
2014-10-19 05:26:16 -0400

    finished callback state

--- main.rb
+++ main.rb
@@ -5,28 +5,28 @@ require 'soundcloud'
 require 'twilio-ruby'
 require 'youtube_search'
 
-def search_by_lyrics(query)
+def search_by_lyrics(query, number)
 	strings = RapGenius.search_by_lyrics(query)[0..2].collect do | song |
 		song.title + " - " + song.artist.name
 	end
 	strings.join("\n")
 end
 
-def search_by_title(query)
+def search_by_title(query, number)
 	strings = RapGenius.search_by_title(query)[0..5].collect do | song |
 		puts song
 	end
 end
 
-def get_url_from_track(query)
+def get_url_from_track(query, number)
 	video = YoutubeSearch.search(query)[0]
 	pp video
 	video_id = video["video_id"]
-	make_call(video_id, video)
+	make_call(video_id, video, number)
 	video
 end
 
-def make_call(video_id, video)
+def make_call(video_id, video, number)
 	# put your own credentials here 
 	account_sid = 'AC39cfed8c7714f14d7d48e462fa809a20' 
 	auth_token = '835f421ebd9035525705eb827d2a9935'  
@@ -35,7 +35,7 @@ def make_call(video_id, video)
 	@client = Twilio::REST::Client.new account_sid, auth_token 
 	 
 	@client.account.calls.create({
-		:to => '12483456497', 
+		:to => number, 
 		:from => '+19182129899', 
 		:url => "http://YouTubeInMP3.com/fetch/?video=http://www.youtube.com/watch?v=" + video_id,
 		:method => 'GET',  
