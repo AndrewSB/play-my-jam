c9ea9117d538439cce0aa7854a087b8e858fcbcc
AndrewSB <asbreckenridge@me.com>
2014-10-20 17:58:27 -0500

    add text your friend feature

--- twilio.rb
+++ twilio.rb
@@ -7,7 +7,13 @@ account_sid = 'AC39cfed8c7714f14d7d48e462fa809a20'
 auth_token = '835f421ebd9035525705eb827d2a9935'
 
 post '/message'  do
-	text = current_production_call(params["Body"], params["From"])
+	if params["Body"].include? "-num"
+		from = params["Body"].index("-num")
+
+		text = current_production_call(params["Body"], params["Body"][from..-1])
+	else
+		text = current_production_call(params["Body"], params["From"])
+	end
 	content_type "text/xml"
 	"<Response>
 		<Message>
