801101b5a6158402a0d997e26a0b91e9c2dcbc80
AndrewSB <asbreckenridge@me.com>
2014-10-21 01:45:35 -0500

    fix the one that got away

--- main.rb
+++ main.rb
@@ -63,9 +63,9 @@ end
 
 def make_call(video_id, video, number)
 	# put your own credentials here 
-	account_sid = 'AC39cfed8c7714f14d7d48e462fa809a20' 
-	auth_token = '835f421ebd9035525705eb827d2a9935'  
-	 
+	account_sid = 'AC39cfed8c7714f14d7d48e462fa809a20'
+	auth_token = '835f421ebd9035525705eb827d2a9935'
+	
 	# set up a client to talk to the Twilio REST API 
 	@client = Twilio::REST::Client.new account_sid, auth_token 
 	 
